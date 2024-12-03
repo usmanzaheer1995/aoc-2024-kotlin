8c21ddbc4124e8e817ee02814dadefcf4b9e667c
Usman Zaheer <usmanzaheer1995@gmail.com>
2024-12-03 11:48:21 +0100

    efficient solution

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -8,29 +8,16 @@ fun main() {
     var safeReports = 0
     Path("src/main/resources/day2_input.txt").forEachLine {
         val row = it.trim().split("\\s+".toRegex()).map(String::toInt)
-        if (isSafe(row)) {
-            println(row)
+        if (isSafeStep1(row)) {
             safeReports++
         }
     }
     println("safeReports = $safeReports")
 }
 
-fun isSafe(list: List<Int>): Boolean {
-    // if difference greater than 3, not safe report
-    if (list[0] == list[1] || abs(list[0] - list[1]) > 3) return false
+fun isSafeStep1(list: List<Int>): Boolean {
+    val isIncreasing = list.zipWithNext().all { (left, right) -> left < right && abs(left - right) <= 3 }
+    val isDecreasing = list.zipWithNext().all { (left, right) -> left > right && abs(left - right) <= 3 }
 
-    var increasing = false
-    var decreasing = false
-
-    if (list[0] < list[1]) increasing = true else decreasing = true
-
-    for (i in 1 until list.size - 1) {
-        if (list[i] > list[i + 1] && increasing) return false
-
-        if (list[i] < list[i + 1] && decreasing) return false
-
-        if (abs(list[i] - list[i + 1]) < 1 || abs(list[i] - list[i + 1]) > 3) return false
-    }
-    return true
+    return isIncreasing || isDecreasing
 }

d2ebdac8fa82086741a70d3d299566c9de1f4f54
Usman Zaheer <usmanzaheer1995@gmail.com>
2024-12-03 11:44:49 +0100

    first attempt

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -9,6 +9,7 @@ fun main() {
     Path("src/main/resources/day2_input.txt").forEachLine {
         val row = it.trim().split("\\s+".toRegex()).map(String::toInt)
         if (isSafe(row)) {
+            println(row)
             safeReports++
         }
     }
@@ -17,7 +18,7 @@ fun main() {
 
 fun isSafe(list: List<Int>): Boolean {
     // if difference greater than 3, not safe report
-    if (abs(list[0] - list[1]) > 3) return false
+    if (list[0] == list[1] || abs(list[0] - list[1]) > 3) return false
 
     var increasing = false
     var decreasing = false
@@ -25,29 +26,11 @@ fun isSafe(list: List<Int>): Boolean {
     if (list[0] < list[1]) increasing = true else decreasing = true
 
     for (i in 1 until list.size - 1) {
-        if (list[i] > list[i + 1] && increasing == true) return false
+        if (list[i] > list[i + 1] && increasing) return false
 
-        if (list[i] < list[i + 1] && decreasing == true) return false
+        if (list[i] < list[i + 1] && decreasing) return false
 
         if (abs(list[i] - list[i + 1]) < 1 || abs(list[i] - list[i + 1]) > 3) return false
     }
     return true
-}
-
-fun isValid(line: List<Int>): Boolean {
-    val isIncreasing = line.zipWithNext().all { (left, right) -> left < right && abs(left - right) <= 3 }
-    val isDecreasing = line.zipWithNext().all { (left, right) -> left > right && abs(left - right) <= 3 }
-
-    return isIncreasing || isDecreasing
-}
-
-fun step1(): Int {
-    var safeReports = 0
-    Path("src/main/resources/day2_input.txt").forEachLine {
-        val row = it.trim().split("\\s+".toRegex()).map(String::toInt)
-        if (isValid(row)) {
-            safeReports++
-        }
-    }
-    return safeReports
 }

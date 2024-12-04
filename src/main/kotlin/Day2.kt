386fe4634dab21396f6d8645055af391ff7055ff
Usman Zaheer <usmanzaheer1995@gmail.com>
2024-12-04 16:53:05 +0100

    day 2 solved

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -5,14 +5,19 @@ import kotlin.io.path.forEachLine
 import kotlin.math.abs
 
 fun main() {
-    var safeReports = 0
+    var safeReportsInStep1 = 0
+    var safeReportsInStep2 = 0
     Path("src/main/resources/day2_input.txt").forEachLine {
         val row = it.trim().split("\\s+".toRegex()).map(String::toInt)
         if (isSafeStep1(row)) {
-            safeReports++
+            safeReportsInStep1++
+        }
+        if (isSafeStep2(row)) {
+            safeReportsInStep2++
         }
     }
-    println("safeReports = $safeReports")
+    println("safeReportsInStep1 = $safeReportsInStep1")
+    println("safeReportsInStep2 = $safeReportsInStep2")
 }
 
 fun isSafeStep1(list: List<Int>): Boolean {
@@ -20,4 +25,16 @@ fun isSafeStep1(list: List<Int>): Boolean {
     val isDecreasing = list.zipWithNext().all { (left, right) -> left > right && abs(left - right) <= 3 }
 
     return isIncreasing || isDecreasing
+}
+
+fun isSafeStep2(list: List<Int>): Boolean {
+    if (isSafeStep1(list)) return true
+
+    for (i in list.indices) {
+        val newList = list.toMutableList()
+        newList.removeAt(i)
+        if (isSafeStep1(newList)) return true
+    }
+
+    return false
 }
